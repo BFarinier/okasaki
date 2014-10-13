b1a15e8324e0dfe9f24ae4a143e852c3fd961ef3
Benjamin <benjamin.farinier@ens-lyon.fr>
2014-10-13 16:17:28 +0200

    First commit

---
+++ BinaryRandomAccessLists/rlist.ml
@@ -0,0 +1,56 @@
+module type S =
+sig
+
+	type 'a rlist
+
+	val empty : 'a rlist
+	val cons : 'a -> 'a rlist -> 'a rlist
+  val head : 'a rlist -> 'a
+  val tail : 'a rlist -> 'a rlist
+end
+
+module RList : S =
+struct
+
+	exception Empty
+
+	type leaf
+	type 'a node = ('a * 'a)
+	type (_,_) tree = Leaf : 'a -> ('a, leaf) tree | Node : ('a,'b) tree * ('a,'b) tree -> ('a,'b node) tree
+
+	type ('a,'b) digit = Zero | One of ('a,'b) tree
+	type ('a,'b) tlist = Null | Cons of ('a,'b) digit * ('a,'b node) tlist
+	type 'a rlist = ('a,leaf) tlist
+
+	let empty = Null
+
+	let rec insert_tree : type a  b. (a,b) tree -> (a,b) tlist -> (a,b) tlist =
+	fun x rlist -> match rlist with
+	| Null -> Cons (One x, Null)
+	| Cons (d, l) ->
+		match d with
+		| Zero -> Cons (One x, l)
+		| One t ->
+			let t = insert_tree (Node (x, t)) l in
+			Cons (Zero, t)
+
+	let cons x l = insert_tree (Leaf x) l
+
+	let rec borrow_tree : type a b. (a,b) tlist -> ((a,b) tree * (a,b) tlist) = function
+	| Null -> raise Empty
+	| Cons (d, l) ->
+		match d with
+		| Zero ->
+			let (Node (t1, t2), l) =  borrow_tree l in
+			(t1, Cons (One t2, l))
+		| One t ->
+			match l with
+			| Null -> (t, Null)
+			| _ -> (t, Cons (Zero, l))
+
+	let head : type a. a rlist -> a = fun l ->
+		let (Leaf l) =  fst (borrow_tree l) in l
+
+	let tail l = snd (borrow_tree l)
+
+end
